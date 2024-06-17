98464be8e3fe314c5116819acb22eeb11940d911
yavuzakyazici <148442912+yavuzakyazici@users.noreply.github.com>
2024-06-17 12:34:30 +0300

    Create expo_android_rotating_video.js

---
+++ expo_android_rotating_video.js
@@ -0,0 +1,24 @@
+import * as ScreenOrientation from "expo-screen-orientation";
+
+  const onFullscreenUpdate = async ({ fullscreenUpdate }) => {
+    switch (fullscreenUpdate) {
+      case 0 || 1:
+        await ScreenOrientation.unlockAsync(); // only on Android required
+        break;
+      case 2 || 3:
+        await ScreenOrientation.lockAsync(
+          ScreenOrientation.OrientationLock.PORTRAIT
+        ); // only on Android required
+        break;
+    }
+  };
+  <Video
+    ref={video}
+    source={{
+      uri: www.example.com,
+    }}
+    useNativeControls
+    resizeMode={ResizeMode.CONTAIN}
+    isLooping
+    onFullscreenUpdate={onFullscreenUpdate}
+  />
